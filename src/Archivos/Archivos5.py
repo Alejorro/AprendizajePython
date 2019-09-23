ad1557530212aa977c2777e3ce2d492794d28bb7
canalessanchezmario@gmail.com <canalessanchezmario@gmail.com>
2019-09-23 15:21:35 +0200

    a

---
+++ src/Archivos/Archivos5.py
@@ -0,0 +1,13 @@
+#llamar a la funcion leer de Archivos5.py
+
+import Archivos3.py as archivos3 
+#Llamada a otro archivo
+
+#Parte2: Montar funciones de lectura y sacarlo por pantalla
+
+
+
+def imprimirPantalla():
+    contenido = Archivos3;
+
+imprimirPantalla()
